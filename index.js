c6696c9276f44d4208b03a6970b48137015d7893
Sven Anders Robbestad <sven@robbestad.com>
2017-01-10 14:18:30 +0100

    using auto-sni

--- index.js
+++ index.js
@@ -5,38 +5,58 @@ const path = require('path');
 const spdy = require('spdy');
 const router = require('./router');
 const app = require('./app');
+const createServer = require("auto-sni");
 
 const ports = {
   http: process.env.HTTP || 80,
   https: process.env.HTTPS || 443
 };
 
-const ssl = {
-  key: process.env.PUBKEY ? fs.readFileSync(process.env.PUBKEY) : fs.readFileSync(path.join(__dirname, 'cert', 'server.key')),
-  cert: process.env.CERT ? fs.readFileSync(process.env.CERT) : fs.readFileSync(path.join(__dirname, 'cert', 'server.cert')),
-};
-
-// SSL options
-const options = {
-  key: ssl.key,
-  cert: ssl.cert,
-  spdy: {
-    protocols: ['h2'],
-    ssl: true,
-    plain: false
+const server = createServer({
+  email: "robbestad@gmail.com", // Emailed when certificates expire.
+  agreeTos: true, // Required for letsencrypt.
+  debug: true, // Add console messages and uses staging LetsEncrypt server. (Disable in production)
+  domains: ["robbestad.no", ["www.robbestad.no", "pwa.robbestad.no"]], // List of accepted domain names. (You can use nested arrays to register bundles with LE).
+  forceSSL: true, // Make this false to disable auto http->https redirects (default true).
+  redirectCode: 301, // If forceSSL is true, decide if redirect should be 301 (permanent) or 302 (temporary). Defaults to 302
+  ports: {
+    http: ports.http, // Optionally override the default http port.
+    https: ports.https // // Optionally override the default https port.
   }
-};
+});
 
-// http server that will only be used as a redirect to SSL
-http.createServer((req, res) => {
-  res.writeHead(301, {
-    "Location": "https://" + (req.headers['host'] + req.url)
-      .replace(ports.http, ports.https)
-      .replace(/:80$/, '')
-      .replace(/:443$/, '')
-  });
-  res.end();
-}).listen(ports.http);
+// Server is a "https.createServer" instance.
+server.once("listening", ()=> {
+  console.log("We are ready to go.");
+});
 
-// http2 server
-spdy.createServer(options, app.callback()).listen(ports.https);
+
+// const ssl = {
+//   key: process.env.PUBKEY ? fs.readFileSync(process.env.PUBKEY) : fs.readFileSync(path.join(__dirname, 'cert', 'server.key')),
+//   cert: process.env.CERT ? fs.readFileSync(process.env.CERT) : fs.readFileSync(path.join(__dirname, 'cert', 'server.crt')),
+// };
+//
+// // SSL options
+// const options = {
+//   key: ssl.key,
+//   cert: ssl.cert,
+//   spdy: {
+//     protocols: ['h2'],
+//     ssl: true,
+//     plain: false
+//   }
+// };
+//
+// // http server that will only be used as a redirect to SSL
+// http.createServer((req, res) => {
+//   res.writeHead(301, {
+//     "Location": "https://" + (req.headers['host'] + req.url)
+//       .replace(ports.http, ports.https)
+//       .replace(/:80$/, '')
+//       .replace(/:443$/, '')
+//   });
+//   res.end();
+// }).listen(ports.http);
+//
+// // http2 server
+// spdy.createServer(options, app.callback()).listen(ports.https);
