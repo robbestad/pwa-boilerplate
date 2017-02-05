21dcb1120c190b0ad4531a6a5dc74e63eacc14c6
Sven Anders Robbestad <sven@robbestad.com>
2017-02-05 19:06:32 +0100

    Added life signs

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,19 +1,27 @@
+var webpack = require('webpack');
 module.exports = {
   entry: './app.jsx',
   output: {
     path: '../public/js',
     filename: 'app.js'
   },
-  module: {
-  rules: [
-    {
-      test: /\.jsx$/,
-      exclude: /(node_modules)/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react']
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
       }
-    }
-  ]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
   }
-}
+};
