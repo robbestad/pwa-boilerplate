3c2842318df7b9fde43872d5daed2fb8f22898a1
Sven Anders Robbestad <sven@robbestad.com>
2017-02-05 20:09:42 +0100

    Added components

--- src/findFace.jsx
+++ src/findFace.jsx
@@ -3,34 +3,8 @@ import classNames from 'classnames';
 import ImageToCanvas from 'imagetocanvas';
 import request from 'superagent';
 const {resizeImage} = require('./helperfncs');
-
-function getOrientation(file, callback) {
-  const reader = new FileReader();
-  reader.onload = function (e) {
-
-    const view = new DataView(e.target.result);
-    if (view.getUint16(0, false) != 0xFFD8) return callback(-2);
-    let length = view.byteLength, offset = 2;
-    while (offset < length) {
-      let marker = view.getUint16(offset, false);
-      offset += 2;
-      if (marker == 0xFFE1) {
-        if (view.getUint32(offset += 2, false) != 0x45786966) return callback(-1);
-        let little = view.getUint16(offset += 6, false) == 0x4949;
-        offset += view.getUint32(offset + 4, little);
-        let tags = view.getUint16(offset, little);
-        offset += 2;
-        for (let i = 0; i < tags; i++)
-          if (view.getUint16(offset + (i * 12), little) == 0x0112)
-            return callback(view.getUint16(offset + (i * 12) + 8, little));
-      }
-      else if ((marker & 0xFF00) != 0xFF00) break;
-      else offset += view.getUint16(offset, false);
-    }
-    return callback(-1);
-  };
-  reader.readAsArrayBuffer(file);
-}
+const {getOrientation} = require('./getOrientation');
+const {serializeImage} = require('./serializeImage');
 
 function toImg(encodedData) {
   const imgElement = document.createElement('img');
@@ -44,27 +18,6 @@ function toPng(canvas) {
   return img;
 }
 
-function serializeImage(dataURL) {
-  const BASE64_MARKER = ';base64,';
-  if (dataURL.indexOf(BASE64_MARKER) == -1) {
-    const parts = dataURL.split(',');
-    const contentType = parts[0].split(':')[1];
-    const raw = decodeURIComponent(parts[1]);
-    return new Blob([raw], {type: contentType});
-  }
-  const parts = dataURL.split(BASE64_MARKER);
-  const contentType = parts[0].split(':')[1];
-  const raw = window.atob(parts[1]);
-  const rawLength = raw.length;
-
-  const uInt8Array = new Uint8Array(rawLength);
-
-  for (let i = 0; i < rawLength; ++i) {
-    uInt8Array[i] = raw.charCodeAt(i);
-  }
-
-  return new Blob([uInt8Array], {type: contentType});
-}
 
 export default class Camera extends React.Component {
   constructor() {
