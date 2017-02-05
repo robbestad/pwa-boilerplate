661fe33c627618eea74a2cc81c652db383b39f08
Sven Anders Robbestad <sven@robbestad.com>
2017-02-05 10:49:34 +0100

    Upped SW version

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,5 +1,5 @@
 // Incrementing CACHE_VERSION will kick off the install event and force previously cached resources to be cached again.
-const CACHE_VERSION = 2;
+const CACHE_VERSION = 3;
 let CURRENT_CACHES = {
   offline: 'offline-v' + CACHE_VERSION
 };
