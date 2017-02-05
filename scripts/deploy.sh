13944700ee3eb5779b5b0efaa845484b55a5311d
Sven Anders Robbestad <sven@robbestad.com>
2017-02-05 20:00:34 +0100

    0.2.0

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-PACKAGE_VERSION=$(node -p -e "require('../package.json').version")
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 git commit -am"Deploy version $PACKAGE_VERSION"
 git push acandogit master && git push origin master
